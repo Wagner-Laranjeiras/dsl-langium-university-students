5350d461e4616bde41d434c0b489d0012ce5a43a
Wagner-Laranjeiras <wlaranjeiras.dev@gmail.com>
2025-08-11 07:38:53 +0200

    Achieve tasks 1 (setup project), 2 (create grammar) and 3 (implement 2 validations)

---
+++ uni-students/packages/language/src/index.ts
@@ -0,0 +1,5 @@
+export * from './university-students-module.js';
+export * from './university-students-validator.js';
+export * from './generated/ast.js';
+export * from './generated/grammar.js';
+export * from './generated/module.js';
---
+++ uni-students/packages/language/src/university-students-validator.ts
@@ -0,0 +1,40 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import { Discipline, Statement, type Student, type UniversityStudentsAstType } from './generated/ast.js';
+import type { UniversityStudentsServices } from './university-students-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: UniversityStudentsServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.UniversityStudentsValidator;
+    const checks: ValidationChecks<UniversityStudentsAstType> = {
+       Student: validator.checkStudent,
+       Statement: validator.checkStatement,
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class UniversityStudentsValidator {
+    checkStudent(student: Student, accept: ValidationAcceptor): void {
+        if (!student.age) return
+        const MIN_AGE: number = 18;
+        // Check for student's age
+        student.age < MIN_AGE
+            ? accept('error', `Student needs to be of age ${MIN_AGE}+`, { node: student, property: 'age' })
+            : null
+    }
+
+    checkStatement(statement: Statement, accept: ValidationAcceptor): void { // it really dos take only those 2 args, tried passing university but it didn't work
+        if (!statement.discipline) return
+        const university = statement.university?.ref; // .ref points to the refs source
+        const universityDisciplines: (Discipline | undefined)[] = university?.disciplines?.map(d => d.ref) ?? [];
+        // Check if discipline exists
+        !universityDisciplines.includes(statement.discipline.ref)
+            ? accept('error', `University ${university} doesn't contain discipline ${statement.discipline}`, { node: statement, property: 'discipline' })
+            : null
+    }
+}
